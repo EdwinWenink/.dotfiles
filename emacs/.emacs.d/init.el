447a41c624507fee605e531c7aeb5cabc8cd5fcf
EdwinWenink <edwinwenink@hotmail.com>
2020-01-13 14:05:13 +0100

    update init.el from Windows

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,6 +14,12 @@
     (setq default-directory "/home/edwin/")
     (setq default-directory "C:/Users/Edwin Wenink/")
 )
+;; Go to notes index
+(global-set-key (kbd "C-c n") (lambda() (interactive) (find-file "C:/Users/Edwin Wenink/Documents/Notes/index.md" )))
+
+;; Default font, see https://www.emacswiki.org/emacs/ChangeFontsPermanentlyOnWindows
+;;(set-face-attribute 'default nil :family "Consolas" :height 110)
+(set-face-font 'default' "PragmataPro Mono Liga-11")
 
 ;; UTF-8 
 (prefer-coding-system 'utf-8)
@@ -53,8 +59,8 @@
 	(setq dashboard-items '((recents  . 5)
 							(bookmarks . 5)
 							(projects . 5)
-							(agenda . 5)
-							(registers . 5)))
+							(agenda . 10)
+							))
     (dashboard-setup-startup-hook)
 	(setq dashboard-set-footer nil)
 	;;(setq dashboard-footer "Dashboard is pretty cool!")
@@ -66,7 +72,11 @@
 	:diminish which-key-mode
 	:config
 	(which-key-mode 1)
+	(add-hook 'org-mode-hook 'which-key-mode)
+	(setq which-key-allow-evil-operators t)
+	which-key-show-operator-state-maps t
 )
+
 
 ;; Doc-view
 (setq doc-view-ghostscript-program "C:/Program Files/gs/gs9.27/bin/gswin64c.exe")
@@ -78,6 +88,14 @@
 (setq vc-make-backup-files t)
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
 
+;; Modeline customization
+(use-package smart-mode-line
+  :ensure t
+  :config
+  (setq sml/no-confirm-load-theme t)
+  (sml/setup)
+) 
+
 ;; Display time in modeline
 (display-time-mode 1)
 
@@ -87,6 +105,34 @@
   :config
   (wc-mode 1)
 )
+
+;; Shorcuts for changing font size
+;; Source: https://github.com/munen/emacs.d
+(defun zoom-in ()
+  (interactive)
+  (let ((x (+ (face-attribute 'default :height)
+              10)))
+    (set-face-attribute 'default nil :height x)))
+
+(defun zoom-out ()
+  (interactive)
+  (let ((x (- (face-attribute 'default :height)
+              10)))
+    (set-face-attribute 'default nil :height x)))
+
+(define-key global-map (kbd "C-1") 'zoom-in)
+(define-key global-map (kbd "C-0") 'zoom-out)
+
+;; Unix style: brief prompt
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Adjust buffer when file is edited by something else
+(global-auto-revert-mode t)
+
+;; Enable winner mode, see https://www.emacswiki.org/emacs/WinnerMode
+;; Toggle through old window configurations with C-c left or right.
+(when (fboundp 'winner-mode)
+  (winner-mode 1))
 
 ;; PACKAGE SPECIFIC SETTINGS
 ;; Put some lengthy ones in specific config files later
@@ -214,26 +260,36 @@
     (evil-define-key 'visual global-map "gS" 'evil-Surround-region))
 )
 
+(if (string-equal system-type "gnu/linux")
+    (setq default-directory "/home/edwin/")
+    (setq default-directory "C:/Users/Edwin Wenink/")
+)
+
 ;; Org mode
 ;; See https://pages.sachachua.com/.emacs.d/Sacha.html#org898cd11
 ;; and https://github.com/tsacha/.emacs.d/blob/master/sacha.org#recent-files
-;; Consider using: https://github.com/Somelauw/evil-org-mode
+;; For quasi quote and , notation: https://stackoverflow.com/questions/9449364/elisp-alist-and-strings-type-confusion
 (use-package org
 	:ensure t
 	:init
+	  (if (string-equal system-type "gnu/linux")
+	    (setq org-directory "~/Dropbox/org/")
+	    (setq org-directory "C:/Users/Edwin Wenink/Dropbox/org/"))
 	  (load "~/.emacs.d/org-config/capture-templates.el")
 	  (setq org-confirm-babel-evaluate nil
 			org-odt-data-dir "~/.emacs.d/elisp/org-mode/etc/"
 			org-odt-styles-dir "~/.emacs.d/elisp/org-mode/etc/styles/"
-			org-directory "~/org/"
 			org-archive-location (concat org-directory "archive.org::datetree/* Finished Tasks") 
 			org-default-notes-file (concat org-directory "notes.org")
+			org-refile-targets `( (,(concat org-directory "inbox.org") :maxlevel . 1)
+					      (,(concat org-directory "active.org") :maxlevel . 3)
+					      (,(concat org-directory "future.org") :maxlevel . 2))
 			org-todo-keywords
-			'((sequence  "TODO(t)" "STARTED(s)" "[ ](T)" "NEXT(n)" "|" "DONE(d)" "[x](D)" "CANCELED(c)"))
+			'((sequence  "TODO(t)" "WAITING(w)" "[ ](T)" "NEXT(n)" "|" "DONE(d)" "[x](D)" "CANCELED(c)"))
 			org-todo-keyword-faces
-			'(("TODO" . org-warning)
+			'(("TODO" . "DarkRed") ;; was org-warning
 			  ("DONE" . "DarkGreen")
-			  ("STARTED" . "DarkOrange")
+			  ("WAITING" . "DarkOrange")
 			  ("CANCELED" . "DarkGreen")
 			  ("DELEGATED" . "BlueViolet")
 			  ("NEXT" . "#008080")
@@ -248,10 +304,14 @@
 			org-src-window-setup 'current-window
 			org-edit-src-content-indentation 0
 			org-fontify-quote-and-verse-blocks t
-	                org-agenda-include-diary t
+	                ;;org-agenda-include-diary t
+			;;org-agenda-start-with-clockreport-mode t
+			org-agenda-show-holidays-birthdays t
 			org-hide-leading-stars t
 			org-return-follows-link t
+			org-clock-clocked-in-display 'frame-title
 			)
+
 	  :bind
 	  (("C-c a" . org-agenda)
 	   ("C-c l" . org-store-link)
@@ -283,6 +343,67 @@
 ;; Prepare stuff for org-export-backends
 (setq org-export-backends '(org latex icalendar html ascii))
 
+;; Supercharge your agenda!
+(use-package org-super-agenda
+  :ensure t
+  :after org
+  :config
+    (org-super-agenda-mode t)
+    (setq org-super-agenda-header-map (make-sparse-keymap)) ;; needed to fix conflict with evil org on 'j'
+    (setq org-super-agenda-groups
+	   '(;; Each group has an implicit boolean OR operator between its selectors.
+	     (:name "Schedule"
+		    :time-grid t)  ; Items that appear on the time grid
+	     (:name "Today"  ; Optionally specify section name
+		    :date today
+		    :scheduled today
+		    :todo "TODAY")  ; Items that have this TODO keyword
+	     (:habit t)
+	     (:name "Due today"
+		    :deadline today)
+	     (:name "Overdue"
+		    :deadline past)
+	     (:name "Due soon"
+		    :deadline future)
+	     (:name "Important"
+		    ;; Single arguments given alone
+		    :tag "bills"
+		    :priority "A")
+	     (:name "Next items"
+		    :time-grid t
+		    :todo ("NEXT"))
+	     (:name "To read"
+		    :tag "read"
+		    :tag "book"
+		    :tag "web"
+		    :tag "article")
+	     (:name "Waiting"
+		    :todo "WAITING")
+	     (:name "Emacs"
+		    :tag "emacs")
+	     ;; Set order of multiple groups at once
+	     ;; Groups supply their own section names when none are given
+	     (:todo "WAITING" :order 8)  ; Set order of this section
+	     (:todo ("SOMEDAY" "TO-READ" "CHECK" "TO-WATCH" "WATCHING")
+		    ;; Show this group at the end of the agenda (since it has the
+		    ;; highest number). If you specified this group last, items
+		    ;; with these todo keywords that e.g. have priority A would be
+		    ;; displayed in that group instead, because items are grouped
+		    ;; out in the order the groups are listed.
+		    :order 9)
+	     (:priority<= "B"
+			  ;; Show this section after "Today" and "Important", because
+			  ;; their order is unspecified, defaulting to 0. Sections
+			  ;; are displayed lowest-number-first.
+			  :order 1)
+	     ;; After the last group, the agenda will display items that didn't
+	     ;; match any of these groups, with the default order position of 99
+
+	     )
+      )
+)
+
+
 ;; Evil bindings for org mode; especially needed this for agenda view
 (use-package evil-org
   :ensure t
@@ -296,7 +417,22 @@
   (evil-org-agenda-set-keys)
 ) 
 
-;; Custom keychords
+;; Org quickstart (adapted from https://github.com/munen/emacs.d)
+(defun org ()
+  "Open main 'org-mode' file and start 'org-agenda' for today."
+  (interactive)
+  (find-file (concat org-directory "inbox.org"))
+  (org-agenda-list)
+  (org-agenda-day-view)
+  (shrink-window-if-larger-than-buffer)
+  (other-window 1)
+)
+
+;; Org Timer Settings, source: https://github.com/munen/emacs.d/blob/master/org-pomodoro.el
+;; Starts 25 min timer on clock in on task
+;; Stop manually with M-x org-timer-stop
+(load "~/.emacs.d/org-config/org-pomodoro")
+
 ;; Exit insert mode by pressing jj within given timeframe
 (use-package key-chord
   :ensure t
@@ -317,7 +453,7 @@
 
 ;; Add evil bindings to magit
 (use-package evil-magit
-	:ensure t
+  :ensure t
 )
 
 ;; Markdown editing
@@ -329,7 +465,74 @@
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "pandoc")
+  :config (add-hook 'markdown-mode-hook (visual-line-mode))
+;; N.B. visual clean is softwrap
 )
+
+;; Latex
+;; References:
+;; https://piotr.is/2010/emacs-as-the-ultimate-latex-editor/
+;; https://nasseralkmim.github.io/notes/2016/08/21/my-latex-environment/
+(use-package tex-site
+  :ensure auctex
+  :mode ("\\.tex\\'" . latex-mode)
+  :config
+  (setq TeX-auto-save t)
+  ;;(setq-default TeX-master nil)
+
+  ;; Update PDF buffers after successful LaTeX runs
+  (add-hook 'TeX-after-TeX-LaTeX-command-finished-hook
+	   #'TeX-revert-document-buffer)
+
+  ;; to use pdfview with auctex
+  (add-hook 'LaTeX-mode-hook 'pdf-tools-install)
+
+  ;; to use pdfview with auctex
+  ;; (setq TeX-view-program-selection '((output-pdf "pdf-tools"))
+  ;;      TeX-source-correlate-start-server t)
+  ;; (setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view")))
+)
+
+;; managing references of e.g.  
+(use-package reftex
+  :ensure t
+  :defer t
+  :config
+  (setq reftex-cite-prompt-optional-args t) ; Prompt for empty optional arguments in cite
+)
+
+;; 
+(use-package ivy-bibtex
+  :ensure t
+  :bind ("C-c b b" . ivy-bibtex)
+  :config
+  (if (string-equal system-type "gnu/linux")
+    (setq bibtex-completion-bibliography 
+          '("/home/edwin/bib/ref.bib")) ;; TODO
+    (setq bibtex-completion-bibliography 
+          '("C:/Users/Edwin Wenink/Bib/Artificial Intelligence.bib"
+          "C:/Users/Edwin Wenink/Bib/Philosophy.bib"))
+   )
+  
+  ;; (setq bibtex-completion-library-path 
+  ;;       '("C:/Users/Nasser/OneDrive/Bibliography/references-pdf"
+  ;;         "C:/Users/Nasser/OneDrive/Bibliography/references-etc"))
+
+  ;; using bibtex path reference to pdf file
+  (setq bibtex-completion-pdf-field "File")
+
+  ;;open pdf with external viwer foxit
+  ;; (setq bibtex-completion-pdf-open-function
+  ;;       (lambda (fpath)
+  ;;         (call-process "C:\\Program Files (x86)\\Foxit Software\\Foxit Reader\\FoxitReader.exe" nil 0 nil fpath)))
+
+  (setq ivy-bibtex-default-action 'bibtex-completion-insert-citation)
+)
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-save-query nil)
+;;(set Tex-PDF-mode t)
 
 ;; Elfeed RSS
 ;; see http://pragmaticemacs.com/emacs/read-your-rss-feeds-in-emacs-with-elfeed/
@@ -351,16 +554,39 @@
 )
 
 ;; extra global binding for loading the feed from database
-(global-set-key (kbd "C-c C-f") 'elfeed-load-db)
+(global-set-key (kbd "C-c ef") 'elfeed-load-db)
 
+;;(set-face-font 'default' "PragmataPro Mono Liga-11")
 (use-package elfeed
   :ensure t
   :config
+  (add-hook 'elfeed-show-mode-hook (lambda()
+				    (visual-line-mode)
+				    (set-face-attribute 'variable-pitch (selected-frame) :font (font-spec :name "PragmataPro Mono Liga-11"))
+					(setq fill-column 120)
+					(setq elfeed-show-entry-switch #'my-show-elfeed)
+				    ))
+
   (setq elfeed-db-directory (concat org-directory "elfeed-db"))
   :bind (:map elfeed-search-mode-map
 	      ("wq" . elfeed-save-db);; the q mapping is taken by evil-collection
 	      ("Q" . elfeed-save-db);; the q mapping is taken by evil-collection
 	      ) 
+)
+
+(defun my-show-elfeed (buffer)
+  (with-current-buffer buffer
+    (setq buffer-read-only nil)
+    (goto-char (point-min))
+    (re-search-forward "\n\n")
+    (fill-individual-paragraphs (point) (point-max))
+    (setq buffer-read-only t))
+  (switch-to-buffer buffer))
+
+(use-package elfeed-goodies
+  :ensure t
+  :init
+  (elfeed-goodies/setup)
 )
 
 ;; use an org file to organise feeds
@@ -378,12 +604,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(org-agenda-files
    (quote
-    ("~/org/habits.org" "~/org/schedule.org" "~/org/emacs.org" "~/org/todo.org")))
+    ("c:/Users/Edwin Wenink/Dropbox/org/habits.org" "c:/Users/Edwin Wenink/Dropbox/org/dates.org" "c:/Users/Edwin Wenink/Dropbox/org/active.org" "c:/Users/Edwin Wenink/Dropbox/org/inbox.org")))
  '(package-selected-packages
    (quote
-    (elfeed-org elfeed markdown-mode wc-mode wcMode key-chord evil-org evil-surround evil-goggles evil-expat evil-visualstar evil-replace-with-register evil-exchange evil-commentary evil-lion evil-collection evil use-package))))
+    (org-super-agenda elfeed-goodies smart-mode-line sml latex-pretty-symbols auctex elfeed-org elfeed markdown-mode wc-mode wcMode key-chord evil-org evil-surround evil-goggles evil-expat evil-visualstar evil-replace-with-register evil-exchange evil-commentary evil-lion evil-collection evil use-package)))
+ '(winner-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/.emacs.d/org-config/capture-templates.el
+++ emacs/.emacs.d/org-config/capture-templates.el
@@ -1,5 +1,12 @@
+;; Later inherit org directory from main init.el
+;;(setq org-directory "C:/Users/Edwin Wenink/Dropbox/org/")
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/todo.org" "Inbox")
+      ;; Change TODO later
+      '(("t" "Inbox (TODO)" entry (file+headline "C:/Users/Edwin Wenink/Dropbox/org/inbox.org" "Inbox")
          "* TODO %? %i\n  %a\n")
-        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+        ("i" "Inbox" entry (file+headline "C:/Users/Edwin Wenink/Dropbox/org/inbox.org" "Inbox")
+         "* %? %i\n  %a\n")
+        ("b" "Book" entry (file+headline "C:/Users/Edwin Wenink/Dropbox/org/books.org" "TO READ")
+         "* %? %i\n  %a\n")
+        ("j" "Journal" entry (file+datetree "C:/Users/Edwin Wenink/Dropbox/org/journal.org")
          "* %?\nEntered on %U\n  %i\n  %a")))
